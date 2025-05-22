78c2633472f33adc483ca42517cae96ad9d4c05e
sarkarsubhrajyoti30 <sarkarsubhrajyoti30@gmail.com>
2025-05-22 15:11:40 +0000

    sorted the issues regarding localization files

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:nidhi_rakshak/src/localization/app_localizations.dart';
+
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 //sample_features which will be deleted later
